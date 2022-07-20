03bd75f2cc392a2db830de5bfe3190546aa31bcb
YungWizzaRd <alexey146@inbox.lv>
2022-07-20 18:41:06 +0300

    designed product page

--- includes/view_product.inc.php
+++ includes/view_product.inc.php
@@ -5,22 +5,80 @@ class ViewProduct extends product{
     public function showProductData(){
         $datas = $this->getProductData();
         if($datas){
+        ?>
+        <div class="row m-3">
+        
+        <?php
          foreach ($datas as $data) {
-            echo $data['SKU'];
-            echo $data['Name'];
-            echo $data['Price'];
-            echo $data['productType'];
+            ?>
+            
+           <table class="table col-sm-3 text-center table border table-borderless m-3">
+            <tr>
+            
+                <td><input type="checkbox" class=".delete-checkbox"></td>
+                <td><?=$data['SKU'];?> </td>
+  
+            </tr>
+          
+            <tr>
+                <td></td>
+                <td><?=$data['Name'];?></td>
+            </tr>
+            <tr>
+            <td></td>
+                <td><?=$data['Price']; ?> $</td>
+            </tr>
+            <tr>
+            <td></td>
+                <td><?=$data['productType'];?></td>
+            </tr>
+            <?php
             if ($data['productType']=='DVD') {
-                echo $data['DVDsize'];
+                
+                ?>
+                 <tr>
+                 <td></td>
+                <td>Size: <?=$data['DVDsize'];?> MB</td>
+            </tr>
+           
+           
+           <?php
+                
             } elseif ($data['productType']=='Book') {
-                echo $data['BookWeight'];
+                ?>
+
+                 <tr>
+                 <td></td>
+                <td>Weight: <?=$data['BookWeight'];?> KG</td>
+            </tr>
+           
+           <?php
+                
             } elseif ($data['productType']=='Furniture') {
-                echo $data['FurnitureHeight'];
-                echo $data['FurnitureWidth'];
-                echo $data['FurnitureLength'];
+                ?>
+                 <tr>
+                 <td></td>
+                <td>Dimensions: <?=$data['FurnitureHeight'];?>x<?=$data['FurnitureWidth'];?>x<?=$data['FurnitureLength'];?></td>
+            </tr>
+           <?php
+                
             }
-            echo "<br />";
-            }
+            
+         ?>
+        </table>
+        <?php
+        }
+        ?>
+        </div>
+        <?php
+        }else{
+            ?>
+
+        <div class='d-flex justify-content-center'>
+            <H5>NO PRODUCT ADDED</H5>
+        </div>
+
+            <?php
         }
     }
     
--- index.php
+++ index.php
@@ -11,13 +11,26 @@ include './includes/view_product.inc.php'
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>Products</title>
+    <title>Product List</title>
     
 </head>
 <body>
+
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <h5 class="my-0 mr-md-auto font-weight-normal">Product list</h5>
+       <button class="btn"><a href="../Scandiweb/add-product" class='p-2 text-dark' >ADD</a></button>
+        <button type="submit" form="delete" class="btn">MASS DELETE</button>
+  </div>
+ 
+  </div class=form>
+  <form>
     <?php
+    
     $product= new ViewProduct();
     $product->showProductData();
     ?>
+    </form>
+  </div>
 </body>
+
 </html>
